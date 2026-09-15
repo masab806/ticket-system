aafe6803328b4640d950d53aff179519c6600d93
Bert154-beep <masabayan15@gmail.com>
2026-09-15 22:17:44 +0500

    feat: added-event-routes

--- backend/controllers/EventTicketController.js
+++ backend/controllers/EventTicketController.js
@@ -77,4 +77,9 @@ class EventTicketController {
             })
         }
     }
+
 } 
+
+module.exports = {
+    EventTicketController
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
+const EventRouter = require("./routes/EventTicket.route")
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors({
     origin: "http://localhost:5173"
 }))
+
+app.use("/api/events", EventRouter)
 
 app.listen(3000, ()=> {
     console.log("Server Is Running!")
